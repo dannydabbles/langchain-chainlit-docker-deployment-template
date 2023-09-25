41a4b857f5faebd807d886ac300b4ee4b5b1cb5a
Danny Key <danny@dannydabbles.com>
2023-09-25 14:35:43 -0700

    Sort of working basic DM, lots of bugs and loops

--- demo_app/main.py
+++ demo_app/main.py
@@ -1,7 +1,7 @@
 """Python file to serve as the frontend"""
 from langchain import OpenAI # LLMMathChain SerpAPIWrapper?
 from langchain.utilities import GoogleSearchAPIWrapper
-from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
+from langchain.agents import initialize_agent, ZeroShotAgent, Tool, AgentExecutor
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory, ReadOnlySharedMemory
@@ -11,22 +11,23 @@ import chainlit as cl
 
 @cl.on_chat_start
 def start():
+    memory = cl.user_session.get("memory")
+    if memory is None:
+        memory = ConversationBufferMemory(memory_key="game_state")
+
     llm = OpenAI(temperature=0, streaming=True)
     llmc = ChatOpenAI(temperature=0, streaming=True)
     search = GoogleSearchAPIWrapper()
 
-    template = """This is a transcript of a D&D game:
+    template = """Summarize the current state of the D&D campaign:
+    {game_state}
 
-    {chat_history}
-
-    Write a summary of updated game state after the player's turn:
+    The player says:
     {input}
     """
 
-    prompt = PromptTemplate(input_variables=["input", "chat_history"], template=template)
-    memory = ConversationBufferMemory(memory_key="chat_history")
+    prompt = PromptTemplate(input_variables=["input", "game_state"], template=template)
     readonlymemory = ReadOnlySharedMemory(memory=memory)
-
     summry_chain = LLMChain(
         llm=llmc,
         prompt=prompt,
@@ -34,9 +35,31 @@ def start():
         memory=readonlymemory,
     )
 
+
+    #template = """You are a Dungeon master trying to decide what to say next to the player. You are having the following thoughts based on the current D&D campaign state.  Respond to the player.
+
+    #Dungeon Master:
+    #{input}
+
+    #D&D Campaign State:
+    #{game_state}
+    #"""
+
+    #prompt = PromptTemplate(input_variables=["input", "game_state"], template=template)
+    #readonlymemory = ReadOnlySharedMemory(memory=memory)
+    #dm_chain = LLMChain(
+    #    llm=llmc,
+    #    prompt=prompt,
+    #    verbose=True,
+    #    memory=memory,
+    #)
+
+    def reply(input):
+        return input
+
     tools = [
         Tool(
-            name="Fact Search",
+            name="D&D Reference",
             func=search.run,
             description="useful for when you need to answer questions about D&D rules, or look up facts for reference. You should ask targeted questions",
         ),
@@ -45,37 +68,53 @@ def start():
             func=summry_chain.run,
             description="useful for when you summarize a conversation. The input to this tool should be a string, representing who will read this summary.",
         ),
+        Tool(
+            name="Dungeon Master's Reply",
+            func=reply,
+            description="useful for when the Dungeon Master is ready to reply to the player.",
+            return_direct=True
+        ),
     ]
 
-    prefix = """Act as a Dungeon Master keeping game notes, interacting with the player and helping move the story forward. You have access to the following tools:"""
-    suffix = """Game History:
-    {chat_history}
+    prefix = """Act as a Dungeon Master having a conversation with a player, interacting with the player and helping move the story forward. Do not impersonate the player. You have access to the following tools:"""
+    suffix = """Reply to the player after they say: {input}
 
-    Player's Turn:
-    {input}
+    D&D Campaign State:
+    {game_state}
 
-    Scratchpad:
+    Dungeon Master Scratchpad:
     {agent_scratchpad}"""
 
     prompt = ZeroShotAgent.create_prompt(
         tools,
         prefix=prefix,
         suffix=suffix,
-        input_variables=["input", "chat_history", "agent_scratchpad"],
+        input_variables=["input", "game_state", "agent_scratchpad"],
     )
 
-    llm_chain = LLMChain(llm=OpenAI(temperature=0), prompt=prompt)
+    llm_chain = LLMChain(llm=llmc, prompt=prompt, verbose=True)
     agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
     agent_chain = AgentExecutor.from_agent_and_tools(
-        agent=agent, tools=tools, verbose=True, memory=memory
+        agent=agent, tools=tools, verbose=True, memory=memory, handle_parsing_errors="Check your output and make sure it conforms!"
     )
 
+    #agent = initialize_agent(
+    #    llm=llmc,
+    #    tools=tools,
+    #    verbose=True,
+    #    memory=memory,
+    #)
+
     cl.user_session.set("agent", agent_chain)
+    cl.user_session.set("memory", memory)
 
 @cl.on_message
 async def main(message: str):
-    agent = cl.user_session.get("agent")  # type: AgentExecutor
+    agent = cl.user_session.get("agent")
     cb = cl.LangchainCallbackHandler(stream_final_answer=True)
 
-    await cl.make_async(agent.run)(message, callbacks=[cb])
+    reply = await cl.make_async(agent.run)(message, callbacks=[cb])
+
+    print(f"Reply: {reply}")
+
 
