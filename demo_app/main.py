62149b4258fb03b1d6e69cfa0e7b04efbb5455dd
Danny Key <danny@dannydabbles.com>
2023-09-29 11:33:18 -0700

    Sort of decent DM

--- demo_app/main.py
+++ demo_app/main.py
@@ -16,14 +16,12 @@ def start():
         memory = ConversationBufferMemory(memory_key="game_state")
 
     llm = OpenAI(temperature=0, streaming=True)
-    llmc = ChatOpenAI(temperature=0, streaming=True)
+    llmc = ChatOpenAI(temperature=0, streaming=True, model="gpt-4")
     search = GoogleSearchAPIWrapper()
 
-    template = """Summarize the current state of the D&D campaign:
-    {game_state}
+    template = """The player just said "{input}".  Summarize the updated state of the game after the player's turn.
 
-    The player says:
-    {input}
+Campaign Summary: {game_state}
     """
 
     prompt = PromptTemplate(input_variables=["input", "game_state"], template=template)
@@ -35,58 +33,116 @@ def start():
         memory=readonlymemory,
     )
 
+    template = """A new D&D is about to begin, get the ball rolling.  As Dungeon Master for this D&D this campaign, what should happen next after a player says "{input}"?
 
-    #template = """You are a Dungeon master trying to decide what to say next to the player. You are having the following thoughts based on the current D&D campaign state.  Respond to the player.
+Campaign Summary: {game_state}
+    """
 
-    #Dungeon Master:
-    #{input}
+    prompt = PromptTemplate(input_variables=["input", "game_state"], template=template)
+    readonlymemory = ReadOnlySharedMemory(memory=memory)
+    start_chain = LLMChain(
+        llm=llmc,
+        prompt=prompt,
+        verbose=True,
+        memory=readonlymemory,
+    )
 
-    #D&D Campaign State:
-    #{game_state}
-    #"""
+    template = """As Dungeon Master for the current D&D 5e campaign, I need to decide what should happen next after a player says "{input}".
 
-    #prompt = PromptTemplate(input_variables=["input", "game_state"], template=template)
-    #readonlymemory = ReadOnlySharedMemory(memory=memory)
-    #dm_chain = LLMChain(
-    #    llm=llmc,
-    #    prompt=prompt,
-    #    verbose=True,
-    #    memory=memory,
-    #)
+Campaign Summary: {game_state}
+    """
 
-    def reply(input):
-        return input
+    prompt = PromptTemplate(input_variables=["input", "game_state"], template=template)
+    readonlymemory = ReadOnlySharedMemory(memory=memory)
+    dm_chain = LLMChain(
+        llm=llmc,
+        prompt=prompt,
+        verbose=True,
+        memory=readonlymemory,
+    )
+
+    template = """As Dungeon Master for the current D&D 5e campaign, what should I reply to a player that just said "{input}"?
+
+Campaign Summary: {game_state}
+    """
+
+    prompt = PromptTemplate(input_variables=["input", "game_state"], template=template)
+    readonlymemory = ReadOnlySharedMemory(memory=memory)
+    reply_chain = LLMChain(
+        llm=llmc,
+        prompt=prompt,
+        verbose=True,
+        memory=readonlymemory,
+    )
+
+    template = """As Dungeon Master for the current D&D 5e campaign, you should ask for a dice check from the player after they say "{input}".  You may choose to reveal the DC for the roll or not.
+
+Campaign Summary: {game_state}
+    """
+
+    prompt = PromptTemplate(input_variables=["input", "game_state"], template=template)
+    readonlymemory = ReadOnlySharedMemory(memory=memory)
+    roll_chain = LLMChain(
+        llm=llmc,
+        prompt=prompt,
+        verbose=True,
+        memory=readonlymemory,
+    )
+
+    # Function to roll X dice with Y sides
+    def roll_dice(x: int, y: int) -> int:
+        return sum([random.randint(1, y) for _ in range(x)])
 
     tools = [
         Tool(
-            name="D&D Reference",
+            name="Dungeons and Dragons Reference",
             func=search.run,
-            description="useful for when you need to answer questions about D&D rules, or look up facts or ideas for reference. You should ask targeted questions.",
+            description="useful for when you need to answer questions about Dungeons and Dragons rules or facts. You should ask targeted questions.",
         ),
         Tool(
-            name="Game State Tracker",
+            name="Campaign State Tracker",
             func=summry_chain.run,
-            description="useful for when you summarize the state of the game or campaign.",
+            description="useful for when you summarize the current state of the game or campaign, when you need to remember what happened in the past, or when you start repeating yourself."
+        ),
+         Tool(
+            name="Campaign Start",
+            func=start_chain.run,
+            description="useful for when the campaign is just getting started.",
         ),
         Tool(
-            name="Dungeon Master's Reply",
-            func=reply,
-            description="useful for when the Dungeon Master is ready to reply to the player.",
+            name="Dungeon Master Thoughts",
+            func=dm_chain.run,
+            description="useful for when a Dungeons and Dragons 5e Dungeon Master is thinking about the campaign.",
             #return_direct=True,
+        ),
+        Tool(
+            name="Dungeon Master Reply",
+            func=reply_chain.run,
+            description="useful for when you, a Dungeons and Dragons 5e Dungeon Master, are deciding how to form a response to the player.",
+            return_direct=True,
+        ),
+        # Tool to roll X dice with Y sides
+        #Tool(
+        #    name="Dice Roller",
+        #    func=roll_dice,
+        #    description="useful for when you need to roll X dice with Y sides to do a check in Dungeons and Dragons 5e.",
+        Tool(
+            name="Dice Check",
+            func=roll_chain.run,
+            description="useful for when you need to ask the player for a dice check in Dungeons and Dragons 5e.",
         ),
     ]
 
-    prefix = """Act as a Dungeon Master. Summarize the game state, think about what can happen next in a fun D&D 5e game, get creative, and finally reply to the user. Do not impersonate the player or take more than one speech turn. You have access to the following tools:"""
-    suffix = """Reply to the player after their speech turn below.  Your reply should take into account the game state, and take into account anything in the scratchpad.
+    prefix = """How would you, a Dungeon Master, reply to the player's message given the current state of the campaign? You have access to the following tools:"""
+    suffix = """You are a Dungeon Master speaking with a player summarizing what just happened in the campaign from the player's perspective. Make the story entertaining with lots of twists and turns. Keep the story moving forward. Make sure the player has all the information they need. Always do what the player asks. Reply directly to the player's message, acting out any characters or events that happen too. Don't repeat past responses to the player.
 
-    Player:
-    {input}
-
-    D&D Campaign State:
-    {game_state}
-
-    Dungeon Master Scratchpad:
-    {agent_scratchpad}"""
+Campaign Summary: {game_state}
+Player's Message: {input}
+Dungeon Master's Notes:
+##############
+{agent_scratchpad}
+##############
+"""
 
     prompt = ZeroShotAgent.create_prompt(
         tools,
@@ -102,9 +158,9 @@ def start():
         tools=tools,
         verbose=True,
         memory=memory,
-        handle_parsing_errors="Check your output and make sure it conforms!",
-        max_iterations=10,
-        early_stopping_method="generate",
+        handle_parsing_errors="Check your output and make sure it makes sense as a response to the player's message.  If it doesn't, try again.",
+        max_iterations=3,
+        #early_stopping_method="generate",
     )
 
     #agent = initialize_agent(
