6fc0660a5ab34f17ed8864c026205c2ec6da6a00
Danny Key <danny@dannydabbles.com>
2023-10-14 22:22:27 -0700

    Working ZeroShotAgent, too slow.

--- demo_app/main.py
+++ demo_app/main.py
@@ -33,7 +33,7 @@ def create_chain(chain_type, llm, input_variables, template_str, memory):
         llm=llm,
         prompt=prompt,
         verbose=True,
-        memory=ReadOnlySharedMemory(memory=memory),
+        memory=ReadOnlySharedMemory(memory=CombinedMemory(memories=memory)),
     )
 
 def roll_dice(x: int, y: int) -> int:
@@ -61,61 +61,26 @@ def get_memory(llm):
         input_key="input",
     )
 
-    combined = CombinedMemory(memories=[memory_buffer, memory_entity])
+    #combined = CombinedMemory(memories=[memory_buffer, memory_entity])
 
-    return combined
+    return [memory_buffer, memory_entity]
 
 def get_conversation_chain(llm, memory, tools):
 
-    prompt = PromptTemplate(
-        input_variables=["input", "buffer", "entities"],
-        template="""As the Dungeon Master for this D&D 5e campaign, you should decide what to say next to the player.
-
-What should I say to the player?:
-{input}
-
-Campaign History:
-{buffer}
-
-Campaign Entities Data:
-
-{entities}
-"""
-    )
-
-    prefix = """Reply to the player's message directly as this one person one shot campaign's Dungeon Master...
-
-You have access to the following tools:
-    """
-    suffix = """You are a Dungeon Master replying to a player's message in a one shot D&D 5e campaign. Only reply as a real Dungeon Master would, and don't ever break character.
-
-Player's message:
-{input}
-
-Campaign History:
-{buffer}
-
-Campaign Entities Data:
-{entities}
-"""
-#{agent_scratchpad}
+#    prompt = PromptTemplate(
+#        input_variables=["input", "buffer", "entities"],
+#        template="""As the Dungeon Master for this D&D 5e campaign, you should decide what to say next to the player.
+#
+#What should I say to the player?:
+#{input}
+#
+#Campaign History:
+#{buffer}
+#
+#Campaign Entities Data:
+#{entities}
 #"""
-    # Dungeon Master Scratchpad: {agent_scratchpad}
-
-    prompt = ZeroShotAgent.create_prompt(tools, prefix=prefix, suffix=suffix, input_variables=["input", "buffer", "entities"])
-    llm_chain = ConversationChain(llm=llm, prompt=prompt, verbose=True, memory=memory)
-
-    agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
-    agent_chain = AgentExecutor.from_agent_and_tools(
-        agent=agent,
-        tools=tools,
-        verbose=True,
-        memory=memory,
-        handle_parsing_errors="As a Dungeon Master, I'll put it a different way...",
-        max_iterations=10,
-        early_stopping_method="generate",
-    )
-
+#    )
     #agent = initialize_agent(
     #    llm=llm,
     #    agent_type=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
@@ -133,6 +98,38 @@ Campaign Entities Data:
 
     #return agent
 
+
+    prefix = """As the Dungeon Master for this D&D 5e campaign, you should decide what you say to the player next after the player's message.  If you're not sure what to say next, use the tools. Your final reply to the player should only include the words the Dungeon Master speaks directly to the player, including any descriptions of what is happening in the campaign this turn.
+
+You have access to the following tools:"""
+    suffix = """You are a Dungeon Master considering what happens next after the player's message.
+
+Player's message:
+{input}
+
+Campaign History:
+{buffer}
+
+Campaign Entities Data:
+{entities}
+
+Dungeon Master's Personal Notes:"""
+    # Dungeon Master Scratchpad: {agent_scratchpad}
+
+    prompt = ZeroShotAgent.create_prompt(tools, prefix=prefix, suffix=suffix, input_variables=["input", "buffer", "entities"])
+    llm_chain = ConversationChain(llm=llm, prompt=prompt, verbose=True, memory=CombinedMemory(memories=[ReadOnlySharedMemory(memory=memory[0]), memory[1]]))
+
+    agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
+    agent_chain = AgentExecutor.from_agent_and_tools(
+        agent=agent,
+        tools=tools,
+        verbose=True,
+        memory=CombinedMemory(memories=memory),
+        handle_parsing_errors="As a Dungeon Master, I'll try a different way...",
+        max_iterations=3,
+        early_stopping_method="generate",
+    )
+
     return agent_chain
 
 def get_llmmath_chain(llm, memory, tools, prompt="You are an AI Dungeon Master for a D&D 5e campaign, what do you say next?"):
@@ -142,7 +139,7 @@ def get_llmmath_chain(llm, memory, tools, prompt="You are an AI Dungeon Master f
         tools=tools,
         memory=memory,
         verbose=True,
-        max_iterations=10,
+        max_iterations=3,
         early_stopping_method="generate",
         handle_parsing_errors="As a Dungeon Master, I'll put it a different way...",
         prompt=prompt,
@@ -155,9 +152,9 @@ def get_llmmath_chain(llm, memory, tools, prompt="You are an AI Dungeon Master f
 def get_tools(llm, memory):
     tools_data = {
         "Dungeon Master Speaks": {
-            "template": """As the Dungeon Master for this D&D 5e one shot campaign, I should decide what to say next to the player. Only reply as a real Dungeon Master would, and don't ever break character. 
+            "template": """As the Dungeon Master for this D&D 5e one shot campaign, I should decide what to say next to the player. Only reply as a real Dungeon Master would, and don't ever break character.  Expand upon the draft if the player needs more information, and make sure you're not repeating yourself.
 
-What should I say to the player?:
+Dungeon Master's Draft Reply:
 {input}
 
 Campaign History:
@@ -166,8 +163,8 @@ Campaign History:
 Campaign Entities Data:
 {entities}
 
-""",
-            "description": "Useful for when the Dungeon Master is done considering what the player said and looking up any information they need. The input to this tool should be what the player said, followed by the Dungeon Master's current notes.  The result of this tool should be the Dungeon Master's response to the player.",
+Dungeon Master:""",
+            "description": "Useful for when the Dungeon Master is done considering what the player said and looking up any information they need. The input to this tool should be a draft of what the Dungeon Master's reply might be.  The result of this tool should be the Dungeon Master's final response to the player, including any descriptions of how the world changed this turn.",
             "input_variables": ["input", "buffer", "entities"],
             "chain_type": ConversationChain,
             "return_direct": True,
@@ -184,7 +181,7 @@ Campaign History:
 Campaign Entities Data:
 {entities}
 
-""",
+Dungeon Master:""",
             "description": "Useful for when the Dungeon Master is unsure of what to do next and needs to consider their options. The input to this tool should be what the Dungeon Master is considering based on what the player said.  The result of this tool should be the Dungeon Master's thoughts on the topic.",
             "input_variables": ["input", "buffer", "entities"],
             "chain_type": ConversationChain,
@@ -201,7 +198,7 @@ Campaign History:
 Campaign Entities Data:
 {entities}
 
-""",
+Dungeon Master:""",
             "description": "Initiates the start of a new D&D campaign, setting the scene and introducing initial plot elements. Useful for when a new campaign or session is starting. The input to this tool should be a unique one sentence made up story description based on what the player just said. The output of this tool should be what the Dungeon Master says to start the campaign.",
             "input_variables": ["input", "buffer", "entities"],
             "chain_type": ConversationChain,
@@ -251,7 +248,6 @@ def start():
     cl.user_session.set("llmc", llmc)
     cl.user_session.set("search", search)
     memory = get_memory(llmc)
-    tools = get_tools(llmc, memory)
     tools = get_tools(llmc, memory)
     agent_chain = get_conversation_chain(llmc, memory, tools)
 
